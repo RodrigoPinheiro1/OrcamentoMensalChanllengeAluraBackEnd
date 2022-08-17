0dd46e1339edd0daeca594111afc87a65093a9ee
RodrigoPinheiro1 <rodrigopsps2015@gmail.com>
2022-08-17 12:25:54 -0300

    Testes Unitarios para Controllers de Usuario de Authentication

---
+++ src/test/java/com/example/orcamentofamiliar/Controllers/AuthenticationControllerTest.java
@@ -0,0 +1,49 @@
+package com.example.orcamentofamiliar.Controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class AuthenticationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    void deveriaDevolver200PassandoInformacoesCorretasDoUsuarioCadastrado() throws Exception {
+        URI uri = new URI("/auth");
+        String json = "{\"email\":\"aaaa@gmail.com\", \"senha\":\"123456\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+    @Test
+    void deveriaDevolverBadRequestPassandoDadosIncoretosDoUsuario() throws Exception {
+        URI uri = new URI("/auth");
+        String json = "{\"email\":\"invalido@gmail.com\", \"senha\":\"123456\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+}
---
+++ src/test/java/com/example/orcamentofamiliar/Controllers/UsuarioControllerTest.java
@@ -0,0 +1,43 @@
+package com.example.orcamentofamiliar.Controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void deveriaDevolver201AoCadastrarUsuario() throws Exception {
+
+        String json = "{\"nome\":\"nome\", \"email\":\"teste@gmail.com\", \"senha\":\"123456\"}";
+        URI uri = new URI("/usuario");
+        mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                .content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+    @Test
+    void deveriaDevolver409PassandoEmailJaExistente () throws Exception {
+
+        String json = "{\"nome\":\"nome\", \"email\":\"aaaa@gmail.com\", \"senha\":\"123456\"}";
+        URI uri = new URI("/usuario");
+        mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                        .content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isConflict());
+    }
+}
